56c0f824a4dbe7d7f2ae09fa07ba90c037761d7e
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-05-27 04:13:54 +0600

    init: problem service

---
+++ problem-service/src/config/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+
+type ServerConfig = {
+  PORT: number
+  DB_URL: string
+}
+
+function loadEnv() {
+    dotenv.config();
+    console.log(`Environment variables loaded`);
+}
+
+loadEnv();
+
+export const serverConfig: ServerConfig = {
+    PORT: Number(process.env.PORT) || 3001,
+    DB_URL: process.env.DB_URL || "mongodb://root:secret@localhost:27017/lcdb?authSource=admin",
+}
