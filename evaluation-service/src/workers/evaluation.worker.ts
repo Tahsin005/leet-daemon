bfb3828175255108a34b5342d580354e041f01bb
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-05-29 01:18:48 +0600

    feat: implement code execution in Docker containers with timeout handling and logging

--- evaluation-service/src/workers/evaluation.worker.ts
+++ evaluation-service/src/workers/evaluation.worker.ts
@@ -1,9 +1,11 @@
 import { Job, Worker } from "bullmq";
 import { SUBMISSION_QUEUE } from "../utils/contants";
 import logger from "../config/logger.config";
-import { EvaluationJob } from "../interfaces/evaluation.interface";
+import { EvaluationJob, EvaluationResult } from "../interfaces/evaluation.interface";
 import { bullmqRedisConnection } from "../config/redis.config";
 import { updateSubmission } from "../api/submission.api";
+import { runCode } from "../utils/containers/codeRunner.util";
+import { LANGUAGE_CONFIG } from "../config/language.config";
 
 async function setupEvaluationWorker() {
   const worker = new Worker(SUBMISSION_QUEUE, async (job: Job) => { 
@@ -16,6 +18,20 @@ async function setupEvaluationWorker() {
 
     try {
         // run the code in a container and evaluate it against the test cases
+        const testCasesRunnerPromise = data.problem.testcases.map(testcase => {
+            return runCode({
+                code: data.code,
+                language: data.language,
+                timeout: LANGUAGE_CONFIG[data.language].timeout,
+                imageName: LANGUAGE_CONFIG[data.language].imageName,
+                input: testcase.input
+            });
+        });
+
+        const testCasesRunnerResults: EvaluationResult[] = await Promise.all(testCasesRunnerPromise);
+        
+        console.log("testCasesRunnerResults", testCasesRunnerResults);
+
         await updateSubmission(data.submissionId, "completed", {});
     } catch (error) {
       logger.error(`Evaluation job failed: ${job}`, error);
